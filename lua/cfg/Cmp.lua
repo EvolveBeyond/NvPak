1007449cdc791a4790091e4e156ab605175145c6
ALipAkrohk <rhpcir@gmail.com>
2022-04-19 01:51:37 +0430

    Optimization

---
+++ lua/cfg/Cmp.lua
@@ -0,0 +1,18 @@
+ 
+
+
+-- Spell check
+ -- require('cmp').setup {
+ -- sources = {
+   -- { name = 'spell' }
+ -- }
+-- }
+ -- vim.opt.spell = true
+ -- vim.opt.spelllang = { 'en_us' } 
+  
+  -- Cmp emoji 
+  require'cmp'.setup {
+  sources = {
+    { name = 'emoji' }
+  }
+}

20cef8d6f4a484696edc65c640b3984c2a056170
ALipAkrohk <rhpcir@gmail.com>
2022-06-03 19:52:59 +0430

    add

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -5,22 +5,22 @@ vim.cmd [[packadd packer.nvim]]
 
 return require('packer').startup(function()
    -- Packer can manage itself
-   use 'wbthomason/packer.nvim'
-  
-   -- treesitter -- 
-   use {
+  use 'wbthomason/packer.nvim'
+
+   -- treesitter --
+  use {
         'nvim-treesitter/nvim-treesitter',
         run = ':TSUpdate'
     }
-   
 
-   use {
+
+  use {
           'VonHeikemen/lsp-zero.nvim',
       requires = {
       -- LSP Support
       {'neovim/nvim-lspconfig'},
       {'williamboman/nvim-lsp-installer'},
-   
+
       -- Autocompletion
       {'hrsh7th/nvim-cmp'},
       {'hrsh7th/cmp-buffer'},
@@ -28,13 +28,15 @@ return require('packer').startup(function()
       {'saadparwaiz1/cmp_luasnip'},
       {'hrsh7th/cmp-nvim-lsp'},
       {'hrsh7th/cmp-nvim-lua'},
-   
+      {'tzachar/cmp-tabnine', run='./install.sh', requires = 'hrsh7th/nvim-cmp'},
+
       -- Snippets
       {'L3MON4D3/LuaSnip'},
       {'rafamadriz/friendly-snippets'},
     }
-  }    
+  }
    -- Themes and more customize Plugins
-   use {'dracula/vim', as = 'dracula'}
+  use {'norcalli/nvim-colorizer.lua'}
+  use {'dracula/vim', as = 'dracula'}
 
 end)

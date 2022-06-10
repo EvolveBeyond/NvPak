ca3ad1298cc941d13a307c281c6110e2bd769ff5
ALipAkrohk <rhpcir@gmail.com>
2022-06-10 04:47:15 +0430

    add nvim-tree and key binfing

--- init.lua
+++ init.lua
@@ -7,10 +7,16 @@ require('plugins')
 -- Treesitter
 require('cfg.treesitter')
 
-
 -- Auto-complete Cmp with the help of tabnine or LSP
 require('cfg.lsp-zero')
 require('cfg.tabnine')
 
+-- File explorer
+require("cfg.tree")
+
 -- Colors and themes
 require("cfg.colorizer")
+
+-- Bindings
+require("cfg.bindings")
+

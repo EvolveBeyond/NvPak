0a73cc6c7c6ef6516d109123a5e73384a79cece7
ALipAkrohk <rhpcir@gmail.com>
2022-06-22 23:40:05 +0430

    fix and optimize lsp cfgs and add new system for load configs and plugins

--- init.lua
+++ init.lua
@@ -4,12 +4,17 @@ require('cfg.basic')
 -- Packer 
 require('plugins')
 
+-- This plugin does several things to speed loading Lua modules and files.
+require('impatient')
+
 -- Treesitter
 require('cfg.treesitter')
 
 -- Auto-complete Cmp with the help of tabnine or LSP
-require('cfg.lsp-zero')
-require('cfg.tabnine')
+require("cfg.lsp-zero")
+require("cfg.cmp")
+require("cfg.figget")
+require("cfg.rust-tools")
 
 -- File explorer
 require("cfg.tree")

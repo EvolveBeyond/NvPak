5e8c5710be5e2265b508d25e78346e2a24e855e9
ALipAkrohk <rhpcir@gmail.com>
2022-07-01 07:34:25 +0430

    fix Session Save location on dashbord.lua

--- lua/default/dashbord.lua
+++ lua/default/dashbord.lua
@@ -1,7 +1,7 @@
   local home = os.getenv('HOME')
   local db = require('dashboard')
   db.preview_command = 'cat | lolcat -F 0.3'
-  db.session_directory = home .. '/.cache/nvim/session'
+  db.session_directory = home .. '/.config/nvim/Session.vim'
   db.preview_file_path = home .. '/.config/nvim/logo.cat'
   db.preview_file_height = 12
   db.preview_file_width = 80

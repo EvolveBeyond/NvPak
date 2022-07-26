3f7855640c679d4439c164d36ae6676f03235c9f
ALipAkrohk <rhpcir@gmail.com>
2022-07-27 03:26:14 +0430

    fix and optimise

--- lua/default/basic.lua
+++ lua/default/basic.lua
@@ -1,19 +1,18 @@
 -- local utf8 = require("utf8")
 -- let g:do_filetype_lua = 1
 
--- packer_bootstrap Finder
-local fn = vim.fn
-local packer_bootstrap = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
+
+-- chack run Packer
+local status_ok, packer = pcall(require, "packer")
 
 -- auto install Plugins and load theme 
-if fn.empty(fn.glob(packer_bootstrap)) == 0 then
-  if require("packer") then  
-    require("packer").install()
+if not status_ok then
+    return
+else
     require("usercustom.usercfg")
-  else
-  vim.cmd 'q'
-  end
 end
+
+
 -- enable mouse support and clipboard(xsel or wl-clipboard(for wayland))
 vim.opt.clipboard = "unnamedplus"
 vim.opt.mouse = "a"
--- lua/plugins.lua
+++ lua/plugins.lua
@@ -3,8 +3,9 @@
 local fn = vim.fn
 local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
 if fn.empty(fn.glob(install_path)) > 0 then
-   fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
+   packer_bootstrap = fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
 end
+
 
 -- Auto Sync Packer after chnage plugin.lua
 vim.cmd([[
@@ -14,11 +15,13 @@ vim.cmd([[
   augroup end
 ]])
 
+vim.cmd [[packadd packer.nvim]]
+
 -- Use a protected call so we don`t error out on first use
-local status_ok, packer = pcall(require, "packer")
-if not status_ok then
+ local status_ok, packer = pcall(require, "packer")
+ if not status_ok then
   return
-end
+ end
 
 -- Move Packer use a popup window
 packer.init {
@@ -29,89 +32,92 @@ packer.init {
   },
 }
 
-vim.cmd [[packadd packer.nvim]]
-
 -- start call Packer list plugin
-return require('packer').startup(function(use)
+local startup = require("packer").startup
 
-  -- Packer can manage itself
-  use 'wbthomason/packer.nvim'
-  -- Speed up loading Lua modules in Neovim to improve startup time.
-  use 'lewis6991/impatient.nvim'
+startup(
+  function(use, use_rocks)
+    -- Packer can manage itself
+    use 'wbthomason/packer.nvim'
+    -- Speed up loading Lua modules in Neovim to improve startup time.
+    use 'lewis6991/impatient.nvim'
 
 
-  -- telescope File Search
-  use {
-    'nvim-telescope/telescope.nvim',
-    requires = { {'nvim-lua/plenary.nvim'} }
+    -- telescope File Search
+    use {
+      'nvim-telescope/telescope.nvim',
+      requires = { {'nvim-lua/plenary.nvim'} }
+        }
+
+    -- treesitter
+    use {'nvim-treesitter/nvim-treesitter', run = ':TSUpdate'}
+
+
+
+    use {'VonHeikemen/lsp-zero.nvim', -- Lsp and Autocompletion System Manager
+        requires = {
+        -- LSP Support
+        {'neovim/nvim-lspconfig'},
+        {'williamboman/nvim-lsp-installer'},
+        {'j-hui/fidget.nvim'},
+
+        -- Autocompletion
+        {'hrsh7th/nvim-cmp'},
+        -- {'tzachar/cmp-tabnine', run='./install.sh', requires = 'hrsh7th/nvim-cmp'}, -- Tabnine Support Plugin
+        {'hrsh7th/cmp-buffer'},
+        {'hrsh7th/cmp-path'},
+        {'saadparwaiz1/cmp_luasnip'},
+        {'hrsh7th/cmp-nvim-lsp'},
+        {'hrsh7th/cmp-nvim-lua'},
+        {'hrsh7th/cmp-cmdline'}, -- NeoVim Comment System Autocomple
+        {'hrsh7th/cmp-nvim-lsp-signature-help'},
+        {'hrsh7th/cmp-nvim-lsp-document-symbol'},
+
+        -- Snippets
+        {'L3MON4D3/LuaSnip'},
+        {'rafamadriz/friendly-snippets'},
       }
-
-  -- treesitter
-  use {'nvim-treesitter/nvim-treesitter', run = ':TSUpdate'}
-
-
-
-  use {'VonHeikemen/lsp-zero.nvim', -- Lsp and Autocompletion System Manager
-      requires = {
-      -- LSP Support
-      {'neovim/nvim-lspconfig'},
-      {'williamboman/nvim-lsp-installer'},
-      {'j-hui/fidget.nvim'},
-
-      -- Autocompletion
-      {'hrsh7th/nvim-cmp'},
-      -- {'tzachar/cmp-tabnine', run='./install.sh', requires = 'hrsh7th/nvim-cmp'}, -- Tabnine Support Plugin
-      {'hrsh7th/cmp-buffer'},
-      {'hrsh7th/cmp-path'},
-      {'saadparwaiz1/cmp_luasnip'},
-      {'hrsh7th/cmp-nvim-lsp'},
-      {'hrsh7th/cmp-nvim-lua'},
-      {'hrsh7th/cmp-cmdline'}, -- NeoVim Comment System Autocomple
-      {'hrsh7th/cmp-nvim-lsp-signature-help'},
-      {'hrsh7th/cmp-nvim-lsp-document-symbol'},
-
-      -- Snippets
-      {'L3MON4D3/LuaSnip'},
-      {'rafamadriz/friendly-snippets'},
     }
-  }
 
-  -- Markdown Previews
-  use({
-      "iamcco/markdown-preview.nvim",
-      run = function() fn["mkdp#util#install"]() end,
-      })
-  -- Rust Code tools
-  use 'simrat39/rust-tools.nvim'
+    -- Markdown Previews
+    use({
+        "iamcco/markdown-preview.nvim",
+        run = function() fn["mkdp#util#install"]() end,
+        })
+    -- Rust Code tools
+    use 'simrat39/rust-tools.nvim'
 
-  -- Debugging System
-  use 'nvim-lua/plenary.nvim'
-  use 'mfussenegger/nvim-dap'
+    -- Debugging System
+    use 'nvim-lua/plenary.nvim'
+    use 'mfussenegger/nvim-dap'
 
-  -- Toggle Terminal
-  use {"akinsho/toggleterm.nvim", tag = 'v1.*', config = function()
-      require("toggleterm").setup()
-    end}
+    -- Toggle Terminal
+    use {"akinsho/toggleterm.nvim", tag = 'v1.*', config = function()
+        require("toggleterm").setup()
+      end}
 
-  -- File Explorer
-  use 'kyazdani42/nvim-tree.lua'
+    -- File Explorer
+    use 'kyazdani42/nvim-tree.lua'
 
-  -- Themes and more customize Plugins
-  use 'norcalli/nvim-colorizer.lua'
-  use 'kyazdani42/nvim-web-devicons'
-  use {'dracula/vim', as = 'dracula'} -- Color theme
-  use 'cocopon/iceberg.vim'  -- color theme
-  use 'joshdick/onedark.vim' -- color theme
-  use 'shaunsingh/nord.nvim' -- color theme
+    -- Themes and more customize Plugins
+    use 'norcalli/nvim-colorizer.lua'
+    use 'kyazdani42/nvim-web-devicons'
+    use {'dracula/vim', as = 'dracula'} -- Color theme
+    use 'cocopon/iceberg.vim'  -- color theme
+    use 'joshdick/onedark.vim' -- color theme
+    use 'shaunsingh/nord.nvim' -- color theme
 
-  -- StaLine Neovim StatusLine
-  use 'tamton-aquib/staline.nvim'
+    -- StaLine Neovim StatusLine
+    use 'tamton-aquib/staline.nvim'
 
-  -- BarBar for Tabline
-  use {
-  'romgrk/barbar.nvim',
-   requires = {'kyazdani42/nvim-web-devicons'}
-      }
-
-end)
+    -- BarBar for Tabline
+    use {
+    'romgrk/barbar.nvim',
+     requires = {'kyazdani42/nvim-web-devicons'}
+        }
+   if packer_bootstrap then
+     require('packer').install()
+   end
+  end
+)
 

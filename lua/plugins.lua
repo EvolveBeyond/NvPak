6f9e314455626c978b96d4693fc2efe5cb969105
ALipAkrohk <rhpcir@gmail.com>
2022-07-29 04:14:07 +0430

    Undo the changes applied to the automatic update system of the plugin file

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -11,7 +11,7 @@ end
 vim.cmd([[
   augroup packer_user_config
     autocmd!
-    autocmd BufWritePost plugins.lua source <afile> | PackerCompile
+    autocmd BufWritePost plugins.lua source <afile> | PackerSync
   augroup end
 ]])
 

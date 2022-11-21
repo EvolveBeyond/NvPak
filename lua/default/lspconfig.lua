4e8f573fd4c1d5285164341d07403217eab81dc8
ALipAkrohk <rhpcir@gmail.com>
2022-11-21 12:51:16 +0330

    update : Update the lspconfig file

--- lua/default/lspconfig.lua
+++ lua/default/lspconfig.lua
@@ -28,7 +28,7 @@ end
 
 -- The nvim-cmp almost supports LSP's capabilities so You should advertise it to LSP servers..
 local capabilities = vim.lsp.protocol.make_client_capabilities()
-capabilities = require('cmp_nvim_lsp').update_capabilities(capabilities)
+capabilities = require('cmp_nvim_lsp').default_capabilities(capabilities)
 
 -- The following example advertise capabilities to `clangd`.
 nvim_lsp.clangd.setup {

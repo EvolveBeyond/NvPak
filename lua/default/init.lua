a8ea5a4d7f0e1dea9c991def69d41617cb918b59
ALipAkrohk <rhpcir@gmail.com>
2022-11-24 16:47:11 +0330

    update : optimization

--- lua/default/init.lua
+++ lua/default/init.lua
@@ -1,11 +1,5 @@
 require('default.packer') -- Plugins
 
--- Cache System
-require('default.packages.impatient')
-
 -- Basic configs
 require('default.basic')
 require('default.bindings')
-
--- LSP
-require('default.lspconfig')

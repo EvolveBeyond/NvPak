9b45991098923d7b134f0d3a16d126a4fd9d7f7a
ALipAkrohk <rhpcir@gmail.com>
2022-12-11 23:01:32 +0330

    update : Save Cursor Position

--- lua/default/basic.lua
+++ lua/default/basic.lua
@@ -41,6 +41,13 @@ end
 -- enable mouse support and clipboard(xsel or wl-clipboard(for wayland))
 vim.opt.clipboard = "unnamedplus"
 vim.opt.mouse = "a"
+-- Restore cursor position
+vim.api.nvim_create_autocmd({ "BufReadPost" }, {
+    pattern = { "*" },
+    callback = function()
+        vim.api.nvim_exec('silent! normal! g`"zv', false)
+    end,
+})
 
 vim.opt.laststatus= 3 -- Status Line Mode
 vim.opt.showtabline= 2 -- Tab Line Mode

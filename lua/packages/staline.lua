68272cfe39b025910a25afc1694c52f46771adaa
ALipAkrohk <rhpcir@gmail.com>
2023-02-11 01:53:48 +0330

    remove : rm barbar and config stabline.

--- lua/packages/staline.lua
+++ lua/packages/staline.lua
@@ -1,18 +1,29 @@
 require("staline").setup({
-    sections = {
-        left = { "  ", "mode", "  ", "lsp_name", "  ", "branch" },
-        mid = { "lsp" },
-        right = { "file_name", "line_column" },
-    },
-    mode_colors = {
-        i = "#ff0040",
-        n = "#ADFBFF",
-        c = "#00ff94",
-        v = "#FFA200",
-    },
-    defaults = {
-        true_colors = true,
-        line_column = " [%l/%L] :%c  ",
-        branch_symbol = " ",
-    },
+	sections = {
+		left = { "  ", "mode", "  ", "lsp_name", "  ", "branch" },
+		mid = { "lsp" },
+		right = { "file_name", "line_column" },
+	},
+	mode_colors = {
+		i = "#ff0040",
+		n = "#ADFBFF",
+		c = "#00ff94",
+		v = "#FFA200",
+	},
+	defaults = {
+		true_colors = true,
+		line_column = " [%l/%L] :%c  ",
+		branch_symbol = " ",
+	},
+})
+
+require("stabline").setup({
+	style = "bubble",
+	bg = "#ff0040",
+	defaults = {
+		true_colors = true,
+	},
+	sections = {
+		mid = { { "RandomHighlight", "-file_name" } },
+	},
 })

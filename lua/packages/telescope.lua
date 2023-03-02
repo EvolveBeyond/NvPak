69a4ec3839a1ee994f9d5a8ef493594ca6009f00
ALipAkrohk <rhpcir@gmail.com>
2023-03-02 12:31:43 +0330

    add trouble to telescope.

--- lua/packages/telescope.lua
+++ lua/packages/telescope.lua
@@ -1,24 +1,15 @@
 -- import telescope plugin safely
 local telescope = require("telescope")
--- import telescope actions safely
-local actions = require("telescope.actions")
+-- Bindings
+local hotkeys = require("packages.bindings.telescope")
 
 -- configure telescope
 telescope.setup({
 	-- configure custom mappings
 	defaults = {
-		mappings = {
-			i = {
-				["<C-k>"] = actions.move_selection_previous, -- move to prev result
-				["<C-j>"] = actions.move_selection_next, -- move to next result
-				["<C-q>"] = actions.send_selected_to_qflist + actions.open_qflist, -- send selected to quickfixlist
-			},
-		},
+		hotkeys.mappings,
 	},
 })
 
 telescope.load_extension("fzf")
 telescope.load_extension("flutter")
-
--- Bindings
-require("packages.bindings.telescope")

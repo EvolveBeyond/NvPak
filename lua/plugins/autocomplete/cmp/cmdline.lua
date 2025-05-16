40483e27a0c7cdf22093a7b7380c42cca23fc9d0
Pakrohk <rhpcir@gmail.com>
2025-05-16 13:22:50 +0330

    optimize cmp and rebuild snippets config!

--- lua/plugins/autocomplete/cmp/cmdline.lua
+++ lua/plugins/autocomplete/cmp/cmdline.lua
@@ -1,21 +1,6 @@
-local function setup_cmdline(cmp)
-	cmp.setup.cmdline("/", {
-		mapping = cmp.mapping.preset.cmdline(),
-		sources = cmp.config.sources({
-			{ name = "nvim_lsp_document_symbol" },
-		}, {
-			{ name = "buffer" },
-		}),
-	})
-
-	cmp.setup.cmdline(":", {
-		mapping = cmp.mapping.preset.cmdline(),
-		sources = cmp.config.sources({
-			{ name = "path" },
-		}, {
-			{ name = "cmdline" },
-		}),
-	})
+local M = {}
+function M.setup_cmdline(cmp)
+  cmp.setup.cmdline('/', { mapping = cmp.mapping.preset.cmdline(), sources = {{name='nvim_lsp_document_symbol'},{name='buffer'}} })
+  cmp.setup.cmdline(':', { mapping = cmp.mapping.preset.cmdline(), sources = {{name='path'},{name='cmdline'}} })
 end
-
-return { setup_cmdline = setup_cmdline }
+return M
--- lua/plugins/autocomplete/cmp/snippets/init.lua
+++
@@ -1,1 +0,0 @@
-require("plugins.autocomplete.cmp.snippets.main")
